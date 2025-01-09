2577bc6bfc3f67cc13b226ec68198c41f5ec3fbb
Englert Ervin  Brunó <englert@users.noreply.github.com>
2025-01-09 03:00:25 +0100

    Add files via upload

---
+++ teglatest_terfogat_test.py
@@ -0,0 +1,24 @@
+import py1
+def test_teglatest_terfogat():
+    assert py1.teglatest_terfogat(1, 2, 3) == 6
+    assert py1.teglatest_terfogat(2, 3, 4) == 24
+    assert py1.teglatest_terfogat(3, 4, 5) == 60
+    assert py1.teglatest_terfogat(4, 5, 6) == 120
+    assert py1.teglatest_terfogat(5, 6, 7) == 210
+    assert py1.teglatest_terfogat(6, 7, 8) == 336
+    assert py1.teglatest_terfogat(7, 8, 9) == 504
+    assert py1.teglatest_terfogat(8, 9, 10) == 720
+    assert py1.teglatest_terfogat(9, 10, 11) == 990
+    assert py1.teglatest_terfogat(10, 11, 12) == 1320
+    assert py1.teglatest_terfogat(11, 12, 13) == 1716
+    assert py1.teglatest_terfogat(12, 13, 14) == 2184
+    assert py1.teglatest_terfogat(13, 14, 15) == 2730
+    assert py1.teglatest_terfogat(14, 15, 16) == 3360
+    assert py1.teglatest_terfogat(15, 16, 17) == 4080
+    assert py1.teglatest_terfogat(16, 17, 18) == 4896
+    assert py1.teglatest_terfogat(17, 18, 19) == 5808
+    assert py1.teglatest_terfogat(18, 19, 20) == 6820
+    assert py1.teglatest_terfogat(19, 20, 21) == 7935
+    assert py1.teglatest_terfogat(20, 21, 22) == 9156
+    assert py1.teglatest_terfogat(21, 22, 23) == 10485
+     
